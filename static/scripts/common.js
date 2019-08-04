067ba9d38d69a8e16fc4d71eeb01d4ca5066daa3
wangzhe1991919 <378098663@qq.com>
2019-08-04 21:04:06 +0800

    生成数据功能更新

--- static/scripts/common.js
+++ static/scripts/common.js
@@ -1,6 +1,6 @@
-const common = {
-    serverUrl : "http://49.234.216.89:8080",
-    //serverUrl : "http://127.0.0.1:8080",
+var common = {
+    //serverUrl : "http://49.234.216.89:8080",
+    serverUrl : "http://127.0.0.1:8080",
     url : {
         getData : this.serverUrl + "/getData"
     }
