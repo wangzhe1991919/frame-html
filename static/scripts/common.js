c593c2d83fa26210d3124edb2ba1a142896e587b
e <378098663@qq.com>
2019-08-15 19:51:37 +0800

    数据类型相关操作

--- static/scripts/common.js
+++ static/scripts/common.js
@@ -2,6 +2,50 @@ var common = {
     //serverUrl : "http://49.234.216.89:8080",
     serverUrl : "http://127.0.0.1:8080",
     url : {
-        getData : this.serverUrl + "/getData"
+        getData : "/getData",
+        insertGenType : "/genManager/insertGenType",
+        deleteGenType : "/genManager/deleteGenType"
+    },
+
+
+    /**
+     * 公共ajax
+     * @param url
+     * @param param
+     * @param type
+     * @param callback  回调函数
+     * @param contentType
+     */
+    ajaxObj : function($,url,param,type,callback,contentType) {
+        if (!type) {
+            type = "POST";
+        }
+        if (!contentType) {
+            contentType = "application/json;charset=utf-8";
+            param = JSON.stringify(param);
+        }
+        $.ajax({
+            type: type,
+            url: common.serverUrl + url,
+            data : param,
+            contentType: contentType,
+            dataType: 'json',
+            success: function(o) {
+                callback(o);
+            }
+        });
+    },
+
+    /**
+     * 参数式ajax请求调用
+     * @param url
+     * @param param
+     * @param type
+     * @param callback
+     */
+    ajaxDefault : function($,url,param,type,callback) {
+        common.ajaxObj($,url,param,type,callback,"application/x-www-form-urlencoded");
     }
+
+
 }
