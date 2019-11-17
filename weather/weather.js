121d8b6ec6d4d8cc136ad85493f0e80f60ea3570
wangzhe1991919 <378098663@qq.com>
2019-11-17 19:30:10 +0800

    天气页面创建（原APP移植）

---
+++ weather/weather.js
@@ -0,0 +1,420 @@
+var sss ={"HeWeather5":[{"aqi":{"city":{"aqi":"88","co":"1","no2":"76","o3":"47","pm10":"87","pm25":"65","qlty":"良","so2":"31"}},"basic":{"city":"天津","cnty":"中国","id":"CN101030100","lat":"39.117000","lon":"117.246000","update":{"loc":"2017-03-23 20:51","utc":"2017-03-23 12:51"}},"daily_forecast":[{"astro":{"mr":"03:00","ms":"13:21","sr":"06:09","ss":"18:26"},"cond":{"code_d":"305","code_n":"305","txt_d":"小雨","txt_n":"小雨"},"date":"2017-03-23","hum":"44","pcpn":"2.2","pop":"100","pres":"1025","tmp":{"max":"11","min":"5"},"uv":"4","vis":"14","wind":{"deg":"118","dir":"东风","sc":"微风","spd":"2"}},{"astro":{"mr":"03:43","ms":"14:19","sr":"06:08","ss":"18:27"},"cond":{"code_d":"305","code_n":"104","txt_d":"小雨","txt_n":"阴"},"date":"2017-03-24","hum":"70","pcpn":"6.0","pop":"100","pres":"1021","tmp":{"max":"8","min":"2"},"uv":"1","vis":"14","wind":{"deg":"110","dir":"东北风","sc":"微风","spd":"1"}},{"astro":{"mr":"04:23","ms":"15:21","sr":"06:06","ss":"18:28"},"cond":{"code_d":"101","code_n":"101","txt_d":"多云","txt_n":"多云"},"date":"2017-03-25","hum":"60","pcpn":"0.5","pop":"79","pres":"1021","tmp":{"max":"12","min":"4"},"uv":"4","vis":"18","wind":{"deg":"76","dir":"西北风","sc":"微风","spd":"5"}}],"hourly_forecast":[{"cond":{"code":"101","txt":"多云"},"date":"2017-03-23 22:00","hum":"51","pop":"46","pres":"1025","tmp":"7","wind":{"deg":"124","dir":"东南风","sc":"3-4","spd":"17"}}],"now":{"cond":{"code":"104","txt":"阴"},"fl":"6","hum":"41","pcpn":"0","pres":"1025","tmp":"8","vis":"7","wind":{"deg":"120","dir":"东南风","sc":"3-4","spd":"11"}},"status":"ok","suggestion":{"air":{"brf":"很差","txt":"气象条件不利于空气污染物稀释、扩散和清除，请尽量避免在室外长时间活动。"},"comf":{"brf":"较舒适","txt":"白天会有降雨，这种天气条件下，人们会感到有些凉意，但大部分人完全可以接受。"},"cw":{"brf":"不宜","txt":"不宜洗车，未来24小时内有雨，如果在此期间洗车，雨水和路上的泥水可能会再次弄脏您的爱车。"},"drsg":{"brf":"较冷","txt":"建议着厚外套加毛衣等服装。年老体弱者宜着大衣、呢外套加羊毛衫。"},"flu":{"brf":"易发","txt":"昼夜温差很大，易发生感冒，请注意适当增减衣服，加强自我防护避免感冒。"},"sport":{"brf":"较不宜","txt":"有降水，推荐您在室内进行健身休闲运动；若坚持户外运动，须注意保暖并携带雨具。"},"trav":{"brf":"适宜","txt":"稍凉，但是有较弱降水和微风作伴，会给您的旅行带来意想不到的景象，适宜旅游，可不要错过机会呦！"},"uv":{"brf":"最弱","txt":"属弱紫外线辐射天气，无需特别防护。若长期在户外，建议涂擦SPF在8-12之间的防晒护肤品。"}}}]}
+var sss1 = {"HeWeather6":[{"basic":{"cid":"CN101030100","location":"天津","parent_city":"天津","admin_area":"天津","cnty":"中国","lat":"39.12559509","lon":"117.19018555","tz":"+8.00"},"update":{"loc":"2019-11-17 18:00","utc":"2019-11-17 10:00"},"status":"ok","now":{"cloud":"97","cond_code":"101","cond_txt":"多云","fl":"6","hum":"42","pcpn":"0.0","pres":"1019","tmp":"10","vis":"5","wind_deg":"317","wind_dir":"西北风","wind_sc":"3","wind_spd":"17"},"daily_forecast":[{"cond_code_d":"104","cond_code_n":"101","cond_txt_d":"阴","cond_txt_n":"多云","date":"2019-11-17","hum":"15","mr":"20:56","ms":"11:12","pcpn":"0.0","pop":"0","pres":"1028","sr":"06:57","ss":"16:54","tmp_max":"12","tmp_min":"-1","uv_index":"2","vis":"25","wind_deg":"352","wind_dir":"北风","wind_sc":"3-4","wind_spd":"18"},{"cond_code_d":"100","cond_code_n":"100","cond_txt_d":"晴","cond_txt_n":"晴","date":"2019-11-18","hum":"18","mr":"22:01","ms":"12:02","pcpn":"0.0","pop":"0","pres":"1027","sr":"06:58","ss":"16:54","tmp_max":"2","tmp_min":"-2","uv_index":"4","vis":"25","wind_deg":"270","wind_dir":"西风","wind_sc":"5-6","wind_spd":"37"},{"cond_code_d":"100","cond_code_n":"100","cond_txt_d":"晴","cond_txt_n":"晴","date":"2019-11-19","hum":"31","mr":"23:10","ms":"12:46","pcpn":"0.0","pop":"0","pres":"1025","sr":"06:59","ss":"16:53","tmp_max":"7","tmp_min":"0","uv_index":"6","vis":"25","wind_deg":"246","wind_dir":"西南风","wind_sc":"1-2","wind_spd":"10"}],"lifestyle":[{"type":"comf","brf":"很不舒适","txt":"白天天阴风大，且天气凉，在这种天气条件下，会感觉很冷，不舒适，外出不宜过久，并注意防风保暖。"},{"type":"drsg","brf":"冷","txt":"天气冷，建议着棉服、羽绒服、皮夹克加羊毛衫等冬季服装。年老体弱者宜着厚棉衣、冬大衣或厚羽绒服。"},{"type":"flu","brf":"易发","txt":"昼夜温差大，风力较强，易发生感冒，请注意适当增减衣服，加强自我防护避免感冒。"},{"type":"sport","brf":"较不宜","txt":"有降水，且风力很强，推荐您选择室内运动；若坚持户外运动，请注意保暖并携带雨具。"},{"type":"trav","brf":"一般","txt":"天气较好，温度适宜，但风比较大，旅游指数一般，外出请注意防暑防风，尽量避免水上活动。"},{"type":"uv","brf":"最弱","txt":"属弱紫外线辐射天气，无需特别防护。若长期在户外，建议涂擦SPF在8-12之间的防晒护肤品。"},{"type":"cw","brf":"适宜","txt":"适宜洗车，未来持续两天无雨天气较好，适合擦洗汽车，蓝天白云、风和日丽将伴您的车子连日洁净。"},{"type":"air","brf":"优","txt":"气象条件非常有利于空气污染物稀释、扩散和清除。"}]}]}
+var global = {
+    aqi : {
+        "优" : "#00FF00",
+        "良" : "#11EE11",
+        "轻度污染" : "#D5D52B",
+        "中度污染" : "#D56F2B",
+        "重度污染" : "#FF4040",
+        "严重污染" : "#FF0000"
+    }
+}
+
+
+//设置dom节点值
+function setHtmlValue(domId,value){
+	var d = document.getElementById(domId);
+	d.innerHTML = value;
+}
+function setImgSrc(domId,src){
+	var d = document.getElementById(domId);
+	d.src=src;
+}
+function setBackGroundColor(domId,rgb){
+	var d = document.getElementById(domId);
+	d.style.backgroundColor= rgb;
+}
+
+function displayTime(){
+	var date = document.getElementById("date");
+	var ndate = document.getElementById("ndate");
+	var time = document.getElementById("time");
+	var week = document.getElementById("week");
+	var o = getCurrentDateTime();
+	date.innerHTML = o.date;
+	ndate.innerHTML = "农历：" + o.ndate;
+	time.innerHTML = o.time;
+	week.innerHTML = o.week;
+
+}
+setInterval("displayTime()",1000);//1000为1秒钟
+
+
+function getWeekPlus(flag) {
+	var d = new Date();
+	var week = d.getDay();
+	if (flag == 1) {
+		if (week == 6) {
+			week = 0;
+		} else {
+			week += 1;
+		}
+
+	} else if (flag == 2) {
+		if (week==5) {
+			week = 0;
+		} else if (week==6) {
+			week = 1;
+		} else {
+			week += 2;
+		}
+	}
+	var weekday = "";
+	if (week == 0)
+	weekday = "星期日";
+	else if (week == 1)
+	weekday = "星期一";
+	else if (week == 2)
+	weekday = "星期二";
+	else if (week == 3)
+	weekday = "星期三";
+	else if (week == 4)
+	weekday = "星期四";
+	else if (week == 5)
+	weekday = "星期五";
+	else if (week == 6)
+	weekday = "星期六";
+	return weekday;
+}
+
+function getCurrentDateTime() {
+var d = new Date();
+var year = d.getFullYear();
+var month = d.getMonth() + 1;
+var date = d.getDate();
+var week = d.getDay();
+
+var currTime = "";
+/*时分秒*/
+var hours = d.getHours();
+var minutes = d.getMinutes();
+var seconds = d.getSeconds();
+var ms = d.getMilliseconds();
+
+var curDateTime = year;
+if (month > 9)
+curDateTime = curDateTime + "年" + month;
+else
+curDateTime = curDateTime + "年0" + month;
+if (date > 9)
+curDateTime = curDateTime + "月" + date + "日";
+else
+curDateTime = curDateTime + "月0" + date + "日";
+
+if (hours > 9)
+currTime = currTime + " " + hours;
+else
+currTime = currTime + " 0" + hours;
+if (minutes > 9)
+currTime = currTime + ":" + minutes;
+else
+currTime = currTime + ":0" + minutes;
+if (seconds > 9)
+currTime = currTime + " <span id='seconds'>" + seconds + "</span>";
+else
+currTime = currTime + " <span id='seconds'>0" + seconds + "</span>";
+
+var weekday = "";
+if (week == 0)
+weekday = "星期日";
+else if (week == 1)
+weekday = "星期一";
+else if (week == 2)
+weekday = "星期二";
+else if (week == 3)
+weekday = "星期三";
+else if (week == 4)
+weekday = "星期四";
+else if (week == 5)
+weekday = "星期五";
+else if (week == 6)
+weekday = "星期六";
+
+var o = new Object();
+o.date = curDateTime;
+o.ndate = showCal();
+o.time = currTime;
+o.week = weekday;
+return o;
+}
+
+
+/*获取当前农历*/
+function showCal(){
+var D=new Date();
+var yy=D.getFullYear();
+var mm=D.getMonth()+1;
+var dd=D.getDate();
+var ww=D.getDay();
+var ss=parseInt(D.getTime() / 1000);
+if (yy<100) yy="19"+yy;
+return GetLunarDay(yy,mm,dd);
+}
+
+//定义全局变量
+var CalendarData=new Array(100);
+var madd=new Array(12);
+var tgString="甲乙丙丁戊己庚辛壬癸";
+var dzString="子丑寅卯辰巳午未申酉戌亥";
+var numString="一二三四五六七八九十";
+var monString="正二三四五六七八九十冬腊";
+var weekString="日一二三四五六";
+var sx="鼠牛虎兔龙蛇马羊猴鸡狗猪";
+var cYear,cMonth,cDay,TheDate;
+CalendarData = new Array(0xA4B,0x5164B,0x6A5,0x6D4,0x415B5,0x2B6,0x957,0x2092F,0x497,0x60C96,0xD4A,0xEA5,0x50DA9,0x5AD,0x2B6,0x3126E, 0x92E,0x7192D,0xC95,0xD4A,0x61B4A,0xB55,0x56A,0x4155B, 0x25D,0x92D,0x2192B,0xA95,0x71695,0x6CA,0xB55,0x50AB5,0x4DA,0xA5B,0x30A57,0x52B,0x8152A,0xE95,0x6AA,0x615AA,0xAB5,0x4B6,0x414AE,0xA57,0x526,0x31D26,0xD95,0x70B55,0x56A,0x96D,0x5095D,0x4AD,0xA4D,0x41A4D,0xD25,0x81AA5,0xB54,0xB6A,0x612DA,0x95B,0x49B,0x41497,0xA4B,0xA164B, 0x6A5,0x6D4,0x615B4,0xAB6,0x957,0x5092F,0x497,0x64B, 0x30D4A,0xEA5,0x80D65,0x5AC,0xAB6,0x5126D,0x92E,0xC96,0x41A95,0xD4A,0xDA5,0x20B55,0x56A,0x7155B,0x25D,0x92D,0x5192B,0xA95,0xB4A,0x416AA,0xAD5,0x90AB5,0x4BA,0xA5B, 0x60A57,0x52B,0xA93,0x40E95);
+madd[0]=0;
+madd[1]=31;
+madd[2]=59;
+madd[3]=90;
+madd[4]=120;
+madd[5]=151;
+madd[6]=181;
+madd[7]=212;
+madd[8]=243;
+madd[9]=273;
+madd[10]=304;
+madd[11]=334;
+
+function GetBit(m,n){
+return (m>>n)&1;
+}
+//农历转换
+function e2c(){
+TheDate= (arguments.length!=3) ? new Date() : new Date(arguments[0],arguments[1],arguments[2]);
+var total,m,n,k;
+var isEnd=false;
+var tmp=TheDate.getYear();
+if(tmp<1900){
+tmp+=1900;
+}
+total=(tmp-1921)*365+Math.floor((tmp-1921)/4)+madd[TheDate.getMonth()]+TheDate.getDate()-38;
+
+if(TheDate.getYear()%4==0&&TheDate.getMonth()>1) {
+total++;
+}
+for(m=0;;m++){
+k=(CalendarData[m]<0xfff)?11:12;
+for(n=k;n>=0;n--){
+if(total<=29+GetBit(CalendarData[m],n)){
+isEnd=true; break;
+}
+total=total-29-GetBit(CalendarData[m],n);
+}
+if(isEnd) break;
+}
+cYear=1921 + m;
+cMonth=k-n+1;
+cDay=total;
+if(k==12){
+if(cMonth==Math.floor(CalendarData[m]/0x10000)+1){
+cMonth=1-cMonth;
+}
+if(cMonth>Math.floor(CalendarData[m]/0x10000)+1){
+cMonth--;
+}
+}
+}
+
+function GetcDateString(){
+var tmp="";
+/*显示农历年：（ 如：甲午(马)年 ）*/
+/*tmp+=tgString.charAt((cYear-4)%10);
+tmp+=dzString.charAt((cYear-4)%12);
+tmp+="(";
+tmp+=sx.charAt((cYear-4)%12);
+tmp+=")年 ";*/
+if(cMonth<1){
+tmp+="(闰)";
+tmp+=monString.charAt(-cMonth-1);
+}else{
+tmp+=monString.charAt(cMonth-1);
+}
+tmp+="月";
+tmp+=(cDay<11)?"初":((cDay<20)?"十":((cDay<30)?"廿":"三十"));
+if (cDay%10!=0||cDay==10){
+tmp+=numString.charAt((cDay-1)%10);
+}
+return tmp;
+}
+
+function GetLunarDay(solarYear,solarMonth,solarDay){
+//solarYear = solarYear<1900?(1900+solarYear):solarYear;
+if(solarYear<1921 || solarYear>2020){
+return "";
+}else{
+solarMonth = (parseInt(solarMonth)>0) ? (solarMonth-1) : 11;
+e2c(solarYear,solarMonth,solarDay);
+return GetcDateString();
+}
+}
+
+
+
+
+
+
+
+
+
+//-------------------------chart   js-----------------------------
+
+function getOption(arr,now) {
+		var xAxisData = [];
+		var seriesData = [];
+
+        if (now) {
+            xAxisData.push("现在");
+            seriesData.push(now.tmp);
+        }
+
+		if (arr != null && arr.length>0) {
+			for (var i = 0; i < arr.length; i++) {
+				var date = arr[i].date;
+				date = date.split(" ")[1];
+				xAxisData.push(date);
+				seriesData.push(arr[i].tmp);
+			}
+		}
+			// 指定图表的配置项和数据
+			var option = {
+				title : {
+					text : '温度预测',
+					textStyle : {
+						fontSize : 15
+					}
+				},
+				legend : {
+					data : ['气温']
+				},
+				tooltip: {
+					trigger: 'axis'
+				},
+				grid : {
+					top : 30,
+					bottom : 30
+				},
+				xAxis: {
+					type : 'category',
+					boundaryGap : false,
+					data:xAxisData.length>0?xAxisData:["0"]
+				},
+				yAxis: {
+					type : 'value',
+					scale : true,
+					splitNumber : 3
+				},
+				series: [{
+					name: '气温',
+					type: 'line',
+					smooth : true,
+					data: seriesData.length>0?seriesData:[0],
+					label: {
+						normal: {
+							show: true,
+							position: 'top',
+							formatter : '{c}℃'
+						}
+					},
+				}]
+			};
+	return option;
+
+}
+
+
+	 // 基于准备好的dom，初始化echarts实例
+   var myChart = echarts.init(document.getElementById('main'),'dark');
+   // 使用刚指定的配置项和数据显示图表。
+   //myChart.setOption(getOption(aaa),true);
+
+
+/**
+ * 设置天气数据
+ * @param str
+ * @param city
+ */
+function setWeatherData(str,city) {
+
+	$.ajax({
+		type: 'GET',
+		async: false,
+		url: "https://free-api.heweather.net/s6/weather/forecast?location=tianjin&key=615a608b5bd64cc7a00f972a8ae33d05",
+		dataType: 'json',
+		success: function(data) {
+			alert(data);
+		}
+	});
+
+    var o = sss1.HeWeather6[0];
+
+	//var aqi = o.aqi;
+	var now = o.now;
+	var today = o.daily_forecast[0];
+	var tomo = o.daily_forecast[1];
+	var aftomo = o.daily_forecast[2];
+	var hourly_forecast = o.hourly_forecast;
+
+    /*if (!aqi||!now||!hourly_forecast) {
+        setHtmlValue("left","当前无网络");
+        return;
+    }*/
+    setHtmlValue("city",o.basic.location);
+	//var o = JSON.parse(str);
+	//设置实时空气质量
+	//setHtmlValue("air",aqi.city.pm25 + " " + aqi.city.qlty);
+	//setBackGroundColor("air",global.aqi[aqi.city.qlty]);
+
+	//设置今天天气
+	setImgSrc("today_img","https://cdn.heweather.com/cond_icon/" + now.cond_code + ".png");
+	setHtmlValue("currTmp",now.tmp);
+	setHtmlValue("currWeather",now.cond_txt);
+	setHtmlValue("today_temp",today.tmp_min + "-" + today.tmp_max + "℃");
+	setHtmlValue("today_cond",today.cond_txt_d==today.cond_txt_n?today.cond_txt_d:(today.cond_txt_d + "转" + today.cond_txt_n));
+
+	var today_wind = today.wind_dir + today.wind_sc;
+	var reg = /^[\u4E00-\u9FA5]+$/;
+	if (!reg.test(today.wind_sc)) {
+		//不是中文，加级
+		today_wind += "级";
+	}
+	setHtmlValue("today_wind",today_wind);
+
+	//设置明天天气
+	setHtmlValue("tomo_week",getWeekPlus(1));
+	setImgSrc("tomo_img","https://cdn.heweather.com/cond_icon/" + tomo.cond_code_d + ".png")
+	setHtmlValue("tomo_temp",tomo.tmp_min + "-" + tomo.tmp_max + "℃");
+	setHtmlValue("tomo_cond",tomo.cond_txt_d==tomo.cond_txt_n?tomo.cond_txt_d:(tomo.cond_txt_d + "转" + tomo.cond_txt_n));
+
+	var tomo_wind = tomo.wind_dir + tomo.wind_sc;
+	var reg = /^[\u4E00-\u9FA5]+$/;
+	if (!reg.test(tomo.wind_sc)) {
+		//不是中文，加级
+		tomo_wind += "级";
+	}
+	setHtmlValue("tomo_wind",tomo_wind);
+
+	//设置后天天气
+	setHtmlValue("aftomo_week",getWeekPlus(2));
+	setImgSrc("aftomo_img","https://cdn.heweather.com/cond_icon/" + aftomo.cond_code_d + ".png")
+	setHtmlValue("aftomo_temp",aftomo.tmp_min + "-" + aftomo.tmp_max + "℃");
+	setHtmlValue("aftomo_cond",aftomo.cond_txt_d==aftomo.cond_txt_n?aftomo.cond_txt_d:(aftomo.cond_txt_d + "转" + aftomo.cond_txt_n));
+
+	var aftomo_wind = aftomo.wind_dir + aftomo.wind_sc;
+	var reg = /^[\u4E00-\u9FA5]+$/;
+	if (!reg.test(aftomo.wind_sc)) {
+		//不是中文，加级
+		aftomo_wind += "级";
+	}
+	setHtmlValue("aftomo_wind",aftomo_wind);
+
+
+	//设置chart数据
+	/*if (hourly_forecast.length >= 1) {
+		var option = getOption(hourly_forecast,now);
+		myChart.setOption(option,true);
+	}*/
+
+}
+
+
