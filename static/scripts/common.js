193532808f2aae72776c7902051bf4970ee1ddfb
wangzhe1991919 <378098663@qq.com>
2019-12-14 22:26:28 +0800

    修改公共ajax方法， code为302时进行跳转

--- static/scripts/common.js
+++ static/scripts/common.js
@@ -1,6 +1,6 @@
 var common = {
     //serverUrl : "http://49.234.216.89:8080",
-    serverUrl : "http://127.0.0.1:8080",
+    serverUrl : "http://49.234.216.89:8080",
     indexPage : "http://49.234.216.89/index.html",
     loginPage : "http://49.234.216.89/login.html",
     url : {
@@ -71,7 +71,7 @@ var common = {
             dataType: 'json',
             success: function(o) {
                 if (o.code == 302) {
-                    window.location.href = common.loginPage;
+                    top.window.location.href = common.loginPage;
                 }
                 if (o.code == 500) {
                     window.location.href = common.loginPage;
